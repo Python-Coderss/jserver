8a65a7fc11f43ead84609fcea4488390a812ffa5
Python-Coderss <101146186+Python-Coderss@users.noreply.github.com>
2023-03-08 18:14:19 -0600

    Delete src directory

--- src/jserver/Config.java
+++
@@ -1,7 +0,0 @@
-package jserver;
-
-public interface Config {
-	public String getServerVersion();
-	public double servletSpec();
-	
-}
--- src/jserver/ImageProvider.java
+++
@@ -1,8 +0,0 @@
-package jserver;
-
-import java.awt.Image;
-
-public interface ImageProvider {
-
-	public Image getImage(int w, int h);
-}
